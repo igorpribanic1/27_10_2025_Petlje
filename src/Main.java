e7c0223b895f457c14abe0b04650b6ab03e3d283
IgorPribanic <igor.pribanic.1@gmail.com>
2025-10-27 19:30:09 +0100

    Drugi commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,47 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
+        int a;
+        int b;
+        int c;
+
+
+
+//        if (a>b){
+//            System.out.println(a);
+//        } else{
+//            System.out.println(b);
+//        }
+        // skraćeni oblik - ternarni operator zamjenjuje gornji if
+        // ako je a > b onda će se u varijablu c zapisati vrijednost varijable a
+        // ako uvjet NIJE zadovoljen tada će se u varijablu c upisati vrijednost varijable b
+        // uvjet može biti napisan (a > b) ili a > b (dakle sa ili bez zagrade - sa zagradom je bolja čitljivost)
+//        c = (a > b) ? a : b;
+//        System.out.println(c);
+
+
+        // Kako pročitati korisnički unos podataka na konzoli
+        // Prilikom definicje scanner klase import (vidi header) se automatski doda
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Upišite prvi cijeli broj: "); // očekuje se unos cijelog broja
+//        a = sc.nextInt();
+//        System.out.print("Upišite drugi cijeli broj: "); // očekuje se unos cijelog broja
+//        b = sc.nextInt();
+
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Upišite prvi cijeli broj: ");
+        a = Integer.parseInt(sc.nextLine()); // očekuje se unos cijelog broja, a nextLine() čita String pa je potrebno taj dio parsati
+        System.out.println("Upišite drugi cijeli broj: ");
+        b = Integer.parseInt(sc.nextLine()); // očekuje se unos cijelog broja, a nextLine() čita String pa je potrebno taj dio parsati
+        c = (a > b) ? a : b;
+        System.out.println("Veći cijeli broj je: " + c);
+
+        // Kada se scanner više neće koristiti u aplikaciji - prestaje se resurs koristiti i više se ne troši memorijski prostor za njega
+        sc.close();
 
     }
 }
---
+++ src/WhileDoWhile.java
@@ -0,0 +1,21 @@
+public class WhileDoWhile {
+    public static void main(String[] args) {
+       int a = 18;
+        // while petlja se NE MORA izvršiti - ako uvjet nije zadovoljen jer se on provjerava prije izvršavanja programa
+//        while (a < 15){
+//            System.out.println(a);
+//            a++; // isto kao i da napišemo a=a+1 ili a+=1- povećava se vrijednost varijable za 1
+//            // a-- smanjuje vrijiednost varijable a za 1 - kao da napišemo a=a-1 ili a-=1
+//        }
+
+
+
+        // do petlja se uvijek izvrši BAREM jednom jer se program izvršava prije provjere uvjeta
+        do {
+            System.out.println(a);
+            a++;
+        } while (a < 15);
+
+
+    }
+}
